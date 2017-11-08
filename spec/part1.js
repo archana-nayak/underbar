2e8eef654e08b2f1de72e3616bed7460548c4a68
archana <archana.nayak@gmail.com>
2017-11-08 15:43:10 -0800

    "Change incomplete test case for uniq in case of sorted array"

--- spec/part1.js
+++ spec/part1.js
@@ -344,9 +344,11 @@
 
       it('should handle iterators that work with a sorted array', function() {
         var iterator = function(value) { return value === 1; };
+        //The iterator returns true only when the value is 1, else it returns false;
+        //So calling iterator on this array will yield [true,false,false,false,false,false];
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(numbers, true, iterator)).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
